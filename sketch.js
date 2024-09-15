31e48d9c66e16c47fa1a78f6a82814a766597e90
DTVPlayer <127330635+DTVPlayer@users.noreply.github.com>
2024-09-15 13:24:39 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(600, 400);
+}
+function draw() {
+
+  background(133, 157, 255);
+  
+  fill("black");
+
+  stroke("white");
+
+  strokeWeight(20);  
+
+  circle(550, 50, 100);
+  
+  fill("yellow");
+  circle(23, 25, 10);
+
+  stroke(0);
+
+  strokeWeight(1);
+
+  fill("grey");
+
+  rect(0, 200, 400, 600);
+}
