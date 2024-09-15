58cffc6b713b7ea2635dbc23de4af36a1fdb4954
DTVPlayer <127330635+DTVPlayer@users.noreply.github.com>
2024-09-15 13:30:02 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,29 @@
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(600, 600);
 }
 function draw() {
 
   background(133, 157, 255);
   
   fill("black");
-
   stroke("white");
-
   strokeWeight(20);  
-
   circle(550, 50, 100);
   
-  fill("yellow");
-  circle(23, 25, 10);
+  fill("purple");
+  noStroke(1);
+  circle(233, 25, 55);
 
-  stroke(0);
+  fill('black');
+  stroke('black');
+  triangle(400, 300, 300, 200, 400, 400);
 
-  strokeWeight(1);
+  strokeWeight(1);  
 
   fill("grey");
 
-  rect(0, 200, 400, 600);
+  rect(0, 200, 600, 400);
+  
+  textSize(30);
+  text("😂", 100, 200);
 }
