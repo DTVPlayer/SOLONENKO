a50edd1e608c5844f319c96f6b21a011718b3c89
DTVPlayer <127330635+DTVPlayer@users.noreply.github.com>
2024-09-15 13:30:33 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,21 +9,15 @@ function draw() {
   stroke("white");
   strokeWeight(20);  
   circle(550, 50, 100);
-  
   fill("purple");
   noStroke(1);
   circle(233, 25, 55);
-
   fill('black');
   stroke('black');
   triangle(400, 300, 300, 200, 400, 400);
-
   strokeWeight(1);  
-
   fill("grey");
-
   rect(0, 200, 600, 400);
-  
   textSize(30);
   text("😂", 100, 200);
 }
